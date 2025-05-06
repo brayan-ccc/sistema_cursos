cbe17a703d89d938d1d88719d8c182f0b1f61ff1
brayan-ccc <brayan.ccc2024@gmail.com>
2025-05-05 21:28:29 -0400

    Agrega modelos para cursos y notas

--- cursos/models.py
+++ cursos/models.py
@@ -1,8 +1,25 @@
 from django.db import models
+from usuarios.models import Usuario
 
-# Create your models here.
 class Curso(models.Model):
-    pass
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+
+    profesor = models.ForeignKey(Usuario,on_delete=models.CASCADE, limit_choices_to={'rol':'profesor'},related_name='cursos_dictados')
+
+    def __str__(self):
+        return f"{self.nombre} - Inicia: {self.fecha_inicio}"
+
 
 class Inscripcion(models.Model):
-    pass
+    estudiante = models.ForeignKey(Usuario,on_delete=models.CASCADE, limit_choices_to={'rol':'estudiante'},related_name='inscrito')
+
+    curso = models.ForeignKey(Curso,on_delete=models.CASCADE)
+
+    fecha_inscripcion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.estudiante.username} Inscrito en: {self.curso.nombre}"
+
--- notas/models.py
+++ notas/models.py
@@ -1,5 +1,23 @@
 from django.db import models
-
+from cursos.models import Inscripcion
 # Create your models here.
-class Notas(models.Model):
-    pass
+class Nota(models.Model):
+    inscripcion = models.OneToOneField(Inscripcion, on_delete=models.CASCADE)
+    nota1 = models.DecimalField(max_digits=5,decimal_places=2)
+    nota2 = models.DecimalField(max_digits=5,decimal_places=2)
+
+    nota_final = models.DecimalField(max_digits=5,decimal_places=2,blank=True,null=True)
+    aprobado = models.BooleanField(default=False)
+
+    def calcular_promedio(self):
+        return (self.nota1 + self.nota2) / 2
+    
+    def save(self,*args, **kwargs):
+        self.nota_final = self.calcular_promedio()
+        self.aprobado = self.nota_final >= 66
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.inscripcion.estudiante.username} - {self.inscripcion.curso.nombre}"
+
+        
